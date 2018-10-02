5ff494190b99a00bb3ad2e4158999f0f6fac7f0f
Ryoung27 <RRYoung89@gmail.com>
2018-10-02 07:58:55 -0500

    finished extension practice

--- Contents.swift
+++ Contents.swift
@@ -1,3 +1,33 @@
 import UIKit
 
-var str = "Hello, playground"
+var myInt = 0
+
+extension Int {
+    mutating func plusOne(){
+        return self + 1
+    }
+}
+
+5.plusOne()
+
+
+var myInt = 10
+myInt.plusOne()
+
+let otherInt = 10
+otherInt.plusOne()
+
+extension Int{
+    func squared() -> Int{
+    return self * self
+    }
+}
+
+let i: Int = 8
+print(i.squared())
+
+func fadeOut(duration: TimeInterval){
+    UIView.animate(withDuration: duration) { [unowned self] in
+        self.alpha = 0
+    }
+}
